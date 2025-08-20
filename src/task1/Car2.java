fa56ac726af8c0c48f68b301d2196cad566d95d4
Nurperi27 <nurperies003@gmail.com>
2025-08-20 12:36:06 +0600

    Update Car2.java

--- src/task1/Car2.java
+++ src/task1/Car2.java
@@ -1,11 +1,11 @@
 package task1;
 
 public class Car2 extends Car{
-    public Car2(){}
+    /*public Car2(){}
 
     public Car2(String model, String color, double maxSpeed) {
         super(model, color, maxSpeed);
-    }
+    }*/
 
     @Override
     public String gas() {
